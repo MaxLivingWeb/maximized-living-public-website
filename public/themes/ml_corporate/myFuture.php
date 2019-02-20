5b09b5b029960cc39def30096d3933398b1b786f
Jose Castillo <jose.castillo@maxliving.com>
2019-02-20 10:13:50 -0500

    Added year to "My Future" MLX Transform copy

--- public/themes/ml_corporate/myFuture.php
+++ public/themes/ml_corporate/myFuture.php
@@ -28,7 +28,7 @@ get_header();
             <div class="myFutureHeroFormIntro">
                 <h2>Get more info.</h2>
                 <p>
-                    Interested in MaxLiving? Let us know! You’ll automatically be entered to win a three-night stay at the Hyatt Regency in Denver, CO for the <a href="https://maxliving.com/events/mlx-transform-chiropractic-continuing-education-conference" target="_blank"><strong>MLX&nbsp;Transform</strong></a> conference June 6 through 8.
+                    Interested in MaxLiving? Let us know! You’ll automatically be entered to win a three-night stay at the Hyatt Regency in Denver, CO for the <a href="https://maxliving.com/events/mlx-transform-chiropractic-continuing-education-conference" target="_blank"><strong>MLX&nbsp;Transform</strong></a> conference June 6 - 8, 2019.
                 </p>
             </div>
 
