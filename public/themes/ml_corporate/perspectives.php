35322263b8dae851c55ee27e53245e2a2020a1a3
Jose Castillo <jose.castillo@maxliving.com>
2019-05-23 13:54:20 -0400

    Composer update, ShortPixel plugin added, Check Email plugin added, enabled plugins to be installed on PROD temporarily to resolve email sending issue.

---
+++ public/themes/ml_corporate/perspectives.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Template Name: Perspectives */
+/**
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package MaxLiving
+ */
+
+get_header();
+
+$showNewsletter = false;
+$mapScripts = true;
+?>
+
+        <?php
+    endif;
+    get_footer();
