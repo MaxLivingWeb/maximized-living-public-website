7e27b9a9cfedd1036df9a62bb26eacb3d8e2f4c8
Jose Castillo <jose.castillo@maxliving.com>
2019-12-26 10:57:03 -0500

    restored yoast update

--- public/themes/ml_core/functions.php
+++ public/themes/ml_core/functions.php
@@ -15,12 +15,6 @@ function filter_plugin_updates( $value ) {
 }
 add_filter( 'site_transient_update_plugins', 'filter_plugin_updates' );
 
-function filter_plugin_updates( $value ) {
-    unset( $value->response['wordpress-seo-premium/wp-seo-premium.php'] );
-    return $value;
-}
-add_filter( 'site_transient_update_plugins', 'filter_plugin_updates' );
-
 /**
  * Tracking
  */
